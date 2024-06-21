6158c931447c7e6e72a78cc5aabcc8400b9e408d
Aymen hm <crosspieloo@gmail.com>
2024-06-20 21:29:17 -0700

    fixed all the error in tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+/**
+ * main - prints the word using puts
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
