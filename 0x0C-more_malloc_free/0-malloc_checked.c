e6f468762f6f499f286f1d9c8286e6e00f21cb51
Aymen hm <crosspieloo@gmail.com>
2024-07-18 21:05:20 -0700

    finishing all the 4 functions of 0x0C

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: The number of bytes to allocate
+ * Return: Pointer to the allocated memory or 98 if it fails
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+	{
+		return (98);
+	}
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This dir contains 4 functions that are about malloc, realloc, calloc and (free) concepts
