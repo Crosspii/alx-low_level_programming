f563685e10abb36457ce8627a8c6198a921a425f
Aymen hm <crosspieloo@gmail.com>
2024-07-22 20:33:27 -0700

    fixin the dog h file more

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,6 @@
  * @age: the age of the dog
  * @owner: the owner of the dog
  */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-}
-
 typedef struct dog
 {
 	char *name;
@@ -24,8 +17,6 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-static int _strlen(const char *s);
-static char *_strcpy(char *dest, const char *stc);
 void free_dog(dog_t *d);
 
 #endif /* DOG_H */
