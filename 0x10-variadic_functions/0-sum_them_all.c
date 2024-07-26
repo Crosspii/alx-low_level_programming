f7c375f57a823b32ed3435bfe635e0976a0804c4
Aymen hm <crosspieloo@gmail.com>
2024-07-25 21:32:23 -0700

    finishin all the taks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,16 +11,15 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	int sum;
+	int sum = 0;
 
 	va_start(ap, n);
-	
+
 	if (n == 0)
 	{
 		return (0);
 	}
 
-	sum = 0;
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(ap, int);
