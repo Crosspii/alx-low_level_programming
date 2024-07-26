5995581f0817d4e0f662d5ba8f1a03e3abad876d
Aymen hm <crosspieloo@gmail.com>
2024-07-25 21:08:23 -0700

    finishing all the tasks in project 0x10

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums up all its parameters
+ * @n: number of parameters
+ * Return: the sum of all the parameters provided
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum;
+
+	va_start(ap, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+	va_end(ap);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+this dir is for learning about variadic_functions and how to use them and create tasks with them
